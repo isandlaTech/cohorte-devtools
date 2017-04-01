34063cb8ba8d1f1677adfca6b32bb94ce93193e3
ogattaz <olivier.gattaz@cohorte-technologies.com>
2017-04-01 23:54:40 +0200

    Share this pde project created to export the javax swing packages to be able to import them in UI pde projects

---
+++ org.cohorte.eclipse.javax.swing.exporter/src/javax/swing/plaf/nimbus/Toto.java
@@ -0,0 +1,14 @@
+/**
+ * File:   Toto.java
+ * Author: "Thomas Calmant"
+ * Date:   25 août 2011
+ */
+package javax.swing.plaf.nimbus;
+
+/**
+ * @author "Thomas Calmant"
+ * 
+ */
+public class Toto {
+
+}
