b6c6f31eee51e28143a93d11cbc7ac60d07b6f42
ogattaz <olivier.gattaz@cohorte-technologies.com>
2017-04-18 16:41:32 +0200

    MOD_OG_20170418 add event handling to allow the programmatic usage of the shutdown command in the current isolate as the herald messages are not delivred to their emmiter add a dependency to eventadmin

--- org.cohorte.eclipse.runner.basic/src/org/cohorte/eclipse/runner/basic/CShutdownGogoCommand.java
+++ org.cohorte.eclipse.runner.basic/src/org/cohorte/eclipse/runner/basic/CShutdownGogoCommand.java
@@ -13,21 +13,32 @@ import org.cohorte.herald.IMessageListener;
 import org.cohorte.herald.Message;
 import org.cohorte.herald.MessageReceived;
 import org.cohorte.herald.NoTransport;
+import org.cohorte.remote.IRemoteServicesConstants;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 import org.psem2m.isolates.base.IIsolateLoggerSvc;
 import org.psem2m.utilities.CXStringUtils;
 
 /**
  * Gogo command to stop all Isolate on Eclipse developpement environement.
  *
+ * MOD_OG_20170418 add event handler to allow the programmatic usage of the
+ * shutdown command in the current isolate as the herald messages are not
+ * delivred to their emmiter
+ *
  * @author bdebbabi
+ * @author ogattaz
  *
  */
 @Component
 @Instantiate
-@Provides(specifications = { CShutdownGogoCommand.class, IMessageListener.class })
-public class CShutdownGogoCommand implements IConstants, IMessageListener {
+@Provides(specifications = { CShutdownGogoCommand.class,
+		IMessageListener.class, EventHandler.class })
+public class CShutdownGogoCommand implements IConstants, IMessageListener,
+		EventHandler {
 
 	/**
 	 * OSGi Bundle Context
@@ -40,12 +51,11 @@ public class CShutdownGogoCommand implements IConstants, IMessageListener {
 
 	/** Herald message filters */
 	@ServiceProperty(name = org.cohorte.herald.IConstants.PROP_FILTERS, value = "{"
-			+ IConstants.SHUTDOWN_MESSAGE + "}")
+			+ RUNNER_BASIC_SHUTDOWN_MESSAGE + "}")
 	private String[] pFilters;
 
 	@Requires
 	private IHerald pHerald;
-
 	/**
 	 * Cohorte Logger service
 	 */
@@ -53,10 +63,23 @@ public class CShutdownGogoCommand implements IConstants, IMessageListener {
 	private IIsolateLoggerSvc pLogger;
 
 	/**
+	 * MOD_OG_20170418 The "pelix.remote.export.reject" property to limit the
+	 * remote export of the services
+	 */
+	@ServiceProperty(name = IRemoteServicesConstants.PROP_EXPORT_REJECT, immutable = true)
+	private final String[] pNotRemote = { IMessageListener.class.getName(),
+			EventHandler.class.getName() };
+
+	/**
 	 * The Gogo commands scope
 	 */
 	@ServiceProperty(name = "osgi.command.scope", value = "runner")
 	private String pScope;
+
+	// MOD_OG_20170418 an array !
+	@ServiceProperty(name = EventConstants.EVENT_TOPIC, value = "{"
+			+ RUNNER_BASIC_SHUTDOWN_TOPIC + "}")
+	private String[] pTopics;
 
 	/**
 	 * Constructor
@@ -80,24 +103,65 @@ public class CShutdownGogoCommand implements IConstants, IMessageListener {
 		try {
 			// wait the broadcast message to be sent
 			Thread.sleep(2000);
-		} catch (InterruptedException e) {
+		} catch (final InterruptedException e) {
 			e.printStackTrace();
 		}
 		try {
 			pBundleContext.getBundle(0).stop();
 			pLogger.logInfo(this, "doShutdown", "bundle 0 stopped");
-		} catch (BundleException e) {
+		} catch (final BundleException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * MOD_OG_20170418
+	 *
+	 * @see org.osgi.service.event.EventHandler#handleEvent(org.osgi.service.event
+	 *      .Event)
+	 */
+	@Override
+	public void handleEvent(Event aEvent) {
+
+		pLogger.logInfo(this, "handleEvent", "receiving a event...");
+
+		try {
+
+			String wEventTopic = aEvent.getTopic();
+
+			pLogger.logInfo(this, "handleEvent", "Event: %s", aEvent);
+
+			if (RUNNER_BASIC_SHUTDOWN_TOPIC.equalsIgnoreCase(wEventTopic)) {
+				doShutdown();
+			}
+
+		} catch (Exception e) {
+
+			if (pLogger == null) {
+				e.printStackTrace();
+			}
+			//
+			else {
+				pLogger.logSevere(this, "handleEvent", "ERROR: %s", e);
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.cohorte.herald.IMessageListener#heraldMessage(org.cohorte.herald.
+	 * IHerald, org.cohorte.herald.MessageReceived)
+	 */
 	@Override
 	public void heraldMessage(final IHerald aHerald,
 			final MessageReceived aMessage) throws HeraldException {
-		pLogger.logInfo(this, "heraldMessage", "receiving a message...");
-		String wSubject = aMessage.getSubject();
+
+		pLogger.logInfo(this, "heraldMessage", "receiving a Herald message...");
+		final String wSubject = aMessage.getSubject();
 		pLogger.logInfo(this, "heraldMessage", "subject=" + "wSubject");
-		if (wSubject.equalsIgnoreCase(SHUTDOWN_MESSAGE)) {
+		if (RUNNER_BASIC_SHUTDOWN_MESSAGE.equalsIgnoreCase(wSubject)) {
 			doShutdown();
 		}
 	}
@@ -109,20 +173,23 @@ public class CShutdownGogoCommand implements IConstants, IMessageListener {
 	public void invalidate() {
 
 		pLogger.logInfo(this, "invalidate", "invalidated");
+
 	}
 
 	/**
 	 * Shutdown Gogo command.
 	 */
 	public void shutdown() {
-		// dispatch SHUTDOWN message to all other isolates
+
 		pLogger.logInfo(this, "shutdown",
-				"sending SHUTDOWN message to all isolates");
+				"Fire SHUTDOWN message to all isolates");
 		try {
-			pHerald.fireGroup("all", new Message(SHUTDOWN_MESSAGE));
-		} catch (NoTransport e) {
+			pHerald.fireGroup("all", new Message(RUNNER_BASIC_SHUTDOWN_MESSAGE));
+		} catch (final NoTransport e) {
 			e.printStackTrace();
 		}
+		// as the method fireGroup send the message to the other but not at the
+		// current isolate !
 		doShutdown();
 	}
 
@@ -132,8 +199,13 @@ public class CShutdownGogoCommand implements IConstants, IMessageListener {
 	@Validate
 	public void validate() {
 
+		pLogger.logInfo(this, "validate", "Validating...");
+
 		pLogger.logInfo(this, "validate", "Commands=[%s]",
 				CXStringUtils.stringTableToString(pCommands));
+
+		pLogger.logInfo(this, "validate", "Validated");
+
 	}
 
 }
--- org.cohorte.eclipse.runner.basic/src/org/cohorte/eclipse/runner/basic/IConstants.java
+++ org.cohorte.eclipse.runner.basic/src/org/cohorte/eclipse/runner/basic/IConstants.java
@@ -1,5 +1,14 @@
 package org.cohorte.eclipse.runner.basic;
 
+/**
+ * @author ogattaz
+ *
+ */
 public interface IConstants {
-	String SHUTDOWN_MESSAGE = "cohorte/eclipse/runner/shutdown";
+
+	// 20170418_OG add a first "slash"
+	String RUNNER_BASIC_SHUTDOWN_MESSAGE = "/cohorte/eclipse/runner/shutdown";
+
+	// 20170418_OG
+	String RUNNER_BASIC_SHUTDOWN_TOPIC = "cohorte-eclipse-runner-shutdown";
 }
