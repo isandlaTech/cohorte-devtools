01b086b392ae1b51ada59f2c120eea918e9d602d
ogattaz <olivier.gattaz@cohorte-technologies.com>
2017-09-26 17:51:53 +0200

    Addition of the export of the sun.security.x509 package for the bundle utilities.crypto

---
+++ org.cohorte.eclipse.javax.other.exporter/src/sun/security/x509/Dummy.java
@@ -0,0 +1,24 @@
+package sun.security.x509;
+
+/**
+ * This class is here to be able to work around an Eclipse PDE issue
+ *
+ * To be able to import a package, this package must be exported by one of the
+ * bundles present in the workspace (target platform or othe pde projects) even
+ * if the package is in the JVM !
+ *
+ *
+ * @author ogattaz
+ *
+ */
+public class Dummy {
+
+	/**
+	 *
+	 */
+	private Dummy() {
+		super();
+		// ...
+	}
+
+}
